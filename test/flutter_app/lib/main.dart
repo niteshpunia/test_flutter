af583033ce7d3b64eb57d2127aa94b2726070589
Nitesh punia <74453768+niteshpunia@users.noreply.github.com>
2021-03-01 18:20:32 +0200

    Update main.dart

--- test/flutter_app/lib/main.dart
+++ test/flutter_app/lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
 void main() {
-  runApp(randomList());
+  runApp(MyApp());
 }
 
-class randomList extends StatelessWidget {
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -15,7 +15,7 @@ class randomList extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Random num gen'),
+      home: MyHomePage(title: 'random num gen'),
     );
   }
 }
@@ -30,56 +30,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   List randomList = [];
+  var randomNum = "";
 
-  getCountries() async {
+  getRandomNumber() async {
     var response =
-    await Dio().get('https://csrng.net/csrng/csrng.php?min=1&max=1000');
+        await Dio().get('https://csrng.net/csrng/csrng.php?min=1&max=1000');
     randomList.add(response.data[0]['random']);
     print(randomList);
+    setState(() {
+      randomNum = response.data[0]['random'].toString();
+    });
     return response.data[0]['random'];
-  }
-
-  Widget myItmes() {
-    return Column(
-      // Text('You CANNOT put other Widgets here'),
-      children: randomList.map((item) => Text(item)).toList(),
-    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
                 child: Text(
-                  'Get New Random Number',
+                  'Get Random Number',
                   style: TextStyle(
-                      fontSize: 30,
+                      fontSize: 40,
                       color: Colors.white,
                       fontWeight: FontWeight.bold),
                 ),
                 onPressed: () {
-                  getCountries();
+                  getRandomNumber();
                 }),
-            Text("Previous NUmbers"),
-            myItmes()
-
+            Text("$randomNum"),
+            Text("History"),
+            ListView.builder(
+              itemCount: randomList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('${randomList[index]}'),
+                );
+              },
+            ),
           ],
         ),
       ),
-
     );
   }
 }
